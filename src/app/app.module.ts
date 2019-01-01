3af9d8c2f038db6bfc155dbfb483d49d861b8d31
Siwar Benamor <siwar.benamor@orange.fr>
2019-01-01 19:42:20 +0100

    Last commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { UserComponent } from './user/user.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProjectsComponent,
+    ContactComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    NgbModule.forRoot()
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
